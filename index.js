49078375e824e06c99b6d68ae01dcfd91737e65d
RV313 <RV.13051995@gmail.com>
2020-08-31 11:30:39 +0530

    Update

--- index.js
+++ index.js
@@ -2,27 +2,18 @@ const expect = require('chai').expect;
 
 
 var get_Reference = require("./get_Color_Pair.js");
-var str_Reference = require('./ToString.js');
+const {ToString} = require("./PairPrint.js");
+const{ testColorToNumber,testNumberToPair} = require("./test.js");
 
+ToString();
 
-str_Reference.ToString();
-
-
-function testNumberToPair(number, expectedMajor, expectedMinor) {
-    const pairOfColors = get_Reference.GetColorFromPairNumber(number);
-    console.log(`${number} = ${expectedMajor} ${expectedMinor}`);
-    expect(pairOfColors.major).equals(expectedMajor);
-    expect(pairOfColors.minor).equals(expectedMinor);
-}
-
-function testColorToNumber(majorColor, minorColor, expectedNumber) {
-    const pairNumber = get_Reference.GetPairNumberFromColors(majorColor, minorColor);
-    console.log(`${pairNumber} = ${majorColor} ${minorColor}`);
-    expect(pairNumber).to.equal(expectedNumber);
-}
 
 console.log("color coder");
 testNumberToPair(4, "white", "brown");
 testNumberToPair(25, "violet", "slate");
 testColorToNumber("black", "brown", 14);
 testColorToNumber("yellow", "green", 18);
+
+
+
+
---
+++ test.js
@@ -0,0 +1,18 @@
+var get_Reference = require("./get_Color_Pair.js");
+
+function testColorToNumber(majorColor, minorColor, expectedNumber) {
+    const pairNumber = get_Reference.GetPairNumberFromColors(majorColor, minorColor);
+    console.log(`${pairNumber} = ${majorColor} ${minorColor}`);
+    expect(pairNumber).to.equal(expectedNumber);
+}
+function testNumberToPair(number, expectedMajor, expectedMinor) {
+    const pairOfColors = get_Reference.GetColorFromPairNumber(number);
+    console.log(`${number} = ${expectedMajor} ${expectedMinor}`);
+    expect(pairOfColors.major).equals(expectedMajor);
+    expect(pairOfColors.minor).equals(expectedMinor);
+}
+
+
+
+module.export = {testColorToNumber : testColorToNumber,
+                testNumberToPair : testNumberToPair};
