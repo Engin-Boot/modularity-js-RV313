3d44cd956aa9839e025364582acfd9f37d77c3bf
Ram_Vaishnav <68468177+RV313@users.noreply.github.com>
2020-08-31 22:50:12 +0530

    Update ToString.js

--- ToString.js
+++ ToString.js
@@ -1,10 +1,11 @@
+var GetColorFromPairNumber_reference = require("./getColorFromPairNumber.js");
 function ToString()
 {   
     console.log("Pin Number" + " " + " Major" + " " + "Minor");
     
     for(var i=1;i<=25;i++)
     {
-        var color_pair =  get_Reference.GetColorFromPairNumber(i);
+        var color_pair =  GetColorFromPairNumber_reference.GetColorFromPairNumber(i);
         console.log(i + "=" + "{" + color_pair.major + " , " + color_pair.minor + "}");
     }
     
